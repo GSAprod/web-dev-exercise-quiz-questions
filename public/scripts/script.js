6b6abe780f0ca0fa59beb0969f826a9f11c3c81c
GSAprod <92989496+GSAprod@users.noreply.github.com>
2023-08-21 22:23:52 +0100

    Fixed buttons transparency when selected

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,9 @@
-$(".btn-correct-answer").click((e) => {
-    $(".btn-correct-answer, .btn-incorrect-answer").prop('disabled', 'true');
+let buttons_active = true
+
+$(".btn-correct-answer").click(function() {
+    if (buttons_active === false) return;
+    buttons_active = false;
+    $(".btn-incorrect-answer").prop('disabled', 'true');
     $(".btn-correct-answer").removeClass("btn-outline-secondary");
     $(".btn-correct-answer").addClass("btn-warning");
 
@@ -7,18 +11,20 @@ $(".btn-correct-answer").click((e) => {
         $(".btn-correct-answer").removeClass("btn-warning").addClass("btn-success");
 
         $(".btn-correct-answer").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    }, 2000);
+    }, 1500);
 })
 
 $(".btn-incorrect-answer").click(function() {
-    $(".btn-correct-answer, .btn-incorrect-answer").prop('disabled', 'true');
+    if (buttons_active === false) return;
+    buttons_active = false
+    $(".btn-correct-answer, .btn-incorrect-answer").not(this).prop('disabled', 'true');
     $(this).removeClass("btn-outline-secondary");
     $(this).addClass("btn-warning");
 
     setTimeout(() => {
         $(this).removeClass("btn-warning").addClass("btn-danger");
-        $(".btn-correct-answer").removeClass("btn-outline-secondary").addClass("btn-success");
+        $(".btn-correct-answer").prop("disabled", false).removeClass("btn-outline-secondary").addClass("btn-success");
 
         $(this).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    }, 2000);
+    }, 1500);
 })
